685a96401dc3d33e586d1510b898b321c3d37d57
kriliou <144113382+kriliou@users.noreply.github.com>
2025-03-21 20:22:16 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  token: "Put-Token-Here",
-  botId: "Bot-Id",
-  buyers: ["276705058370879489"],
+  token: "MTM1MjcyMzAyMTU3NTQ4NzQ4OQ.GeU_2t.yj_1rP5U9s4DPH93T2XgQSsKs8IxR0XJDslQ18",
+  botId: "1352723021575487489",
+  buyers: ["1249433930067611681"],["994864493429530684"]
   prefix: "+",
   color: "#2b2d31",
 };
